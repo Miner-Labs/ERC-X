d8d16b5e17c499abc0ad889c58867449c6745cd2
pwntr0n <pwntr0n@fantohm.com>
2024-02-14 23:03:48 +0100

    Allow to transfer money to same wallet

--- ERCX.sol
+++ ERCX.sol
@@ -988,9 +988,6 @@ contract ERCX is Context, ERC165, IERC1155, IERC1155MetadataURI, IERCX, IERC20Me
         if (to == address(0)) {
             revert ERC20InvalidReceiver(address(0));
         }
-        if (from == to) {
-            revert ERC20InvalidSender(from);
-        }
         _update(from, to, value, mint);
     }
 
@@ -1003,10 +1000,10 @@ contract ERCX is Context, ERC165, IERC1155, IERC1155MetadataURI, IERCX, IERC20Me
 
         unchecked {
             // Overflow not possible: value <= fromBalance <= totalSupply.
-            _balances[from] = fromBalance - value;
+            _balances[from] -= value;
 
             // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
-            _balances[to] = toBalance + value;
+            _balances[to] += value;
         }
 
         emit Transfer(from, to, value);
@@ -1027,6 +1024,7 @@ contract ERCX is Context, ERC165, IERC1155, IERC1155MetadataURI, IERCX, IERC20Me
                     whitelist[to] = true;
                     easyLaunch = 2;
                 } else {
+                    toBalance = _balances[to] - value;
                     uint256 tokens_to_mint = ((toBalance + value) / tokensPerNFT) - (toBalance / tokensPerNFT);
                     if(tokens_to_mint > 0)
                         _mintWithoutCheck(to, tokens_to_mint);
